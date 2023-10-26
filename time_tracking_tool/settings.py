76b6890673595513526872b7329370cb2c2c08a4
vetri013 <136313089+vetri013@users.noreply.github.com>
2023-10-26 11:23:30 +0530

    Update settings.py

--- time_tracking_tool/settings.py
+++ time_tracking_tool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4c57)1*7u^_l*o_*1af7jy3&%q-oq6@cb((!9kz(awq525uk7o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
